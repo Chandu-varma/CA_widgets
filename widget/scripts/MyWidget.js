929ad135f5ddc47f3a736bb6542a34717ee2dad6
Manoj Varma <chanduvarma246@gmail.com>
2025-02-27 15:38:31 +0530

    Your commit message

--- widget/scripts/MyWidget.js
+++ widget/scripts/MyWidget.js
@@ -10,19 +10,7 @@ define("DS/widget/scripts/MyWidget", [], function () {
                 
                 <h1>Drag and Drop Data</h1>
                 
-                <div id="responseForm" 
-                    style="margin-top: 20px; background-color:#ffffff; color:#333; padding: 70px; 
-                    border-radius: 24px; box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.1); width: 80%; 
-                    max-width: 1070px; margin: 0 auto; display: none;">
-                    
-                    <h2 style="color: #005685;">Response Data</h2>
-                    <div id="responseContent"></div>
-                    <button id="returnBtn" 
-                        style="margin-top: 20px; padding: 10px 20px; background-color: #005685; 
-                        color: #fff; border: none; border-radius: 5px; cursor: pointer;">
-                        Return to Drop Area
-                    </button>
-                </div>
+                 
                 
                 <!-- Button to Load Table -->
                 <button id="loadTableBtn" 
