86f0e186acec576e3a72228d35ee28a8f18d96e6
Manoj Varma <chanduvarma246@gmail.com>
2025-02-28 13:15:01 +0530

    Your commit message

--- widget/scripts/MyWidget.js
+++ widget/scripts/MyWidget.js
@@ -1,4 +1,4 @@
-define("DS/widget/scripts/MyWidget", ["DS/widget/scripts/TableWidget"], function (tableModule) {
+define("DS/widget/scripts/MyWidget", ["DS/widget/scripts/TableWidget", "DS/DataDragAndDrop/DataDragAndDrop"], function (tableModule, dragDrop) {
     'use strict';
 
     var page1 = {
@@ -14,15 +14,31 @@ define("DS/widget/scripts/MyWidget", ["DS/widget/scripts/TableWidget"], function
                         style="margin-top: 20px; padding: 10px 20px; background-color: #28a745; 
                         color: #fff; border: none; border-radius: 5px; cursor: pointer;">
                         Load Table
-                    </button>
-
+                    </button><br><br>
+<buttonid="drop" style="margin-top: 20px; padding: 10px 20px; background-color: #28a745>drop here</button>
                     <div id="tableContainer" style="margin-top: 20px;"></div>
                 </div>`;
 
             // Add event listener to button to call TableWidget.js
             document.getElementById("loadTableBtn").addEventListener("click", tableModule.generateTable);
-        }
+            document.getElementById("drop").addEventListener("click", page1.drop);
+        },
+
+
+        drop: function () {
+            var area = document.querySelector("#drop");
+            dragDrop.droppable(area, {
+                function(data) {
+                    alert(data);
+                },
+            },);
+
+        },
+
     };
 
     widget.addEvent("onLoad", page1.onLoad);
+
+
+
 });
