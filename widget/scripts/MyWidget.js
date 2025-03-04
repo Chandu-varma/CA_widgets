a3d3276c2d87111de1538d19e839ef137239257d
Manoj Varma <chanduvarma246@gmail.com>
2025-03-04 12:52:18 +0530

    Your commit message

--- widget/scripts/MyWidget.js
+++ widget/scripts/MyWidget.js
@@ -1,6 +1,12 @@
 define("DS/widget/scripts/MyWidget", ["DS/widget/scripts/TableWidget", "DS/DataDragAndDrop/DataDragAndDrop"], function (tableModule, dragDrop) {
     'use strict';
-
+    var structure = {
+        name: "platformid",
+        type: "list",
+        label: CAAWebAppsUPDyn.get("PlatformId_lbl"),
+        options: [],
+        onchange: "onPlatformIdChange"
+    };
     var page1 = {
         onLoad: function () {
             widget.body.innerHTML = `
@@ -27,6 +33,8 @@ define("DS/widget/scripts/MyWidget", ["DS/widget/scripts/TableWidget", "DS/DataD
                 </div>`;
 
             // Add event listeners
+
+            widget.addPreferences(structure);
             document.getElementById("loadTableBtn").addEventListener("click", tableModule.generateTable);
             document.getElementById("drop").addEventListener("click", page1.drop);
         },
